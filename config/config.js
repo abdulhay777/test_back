7b23fa24e1994a18f0c5ff5a477d95fd39106050
Abdulhay <75833188+abdulhay777@users.noreply.github.com>
2023-04-24 13:54:13 +0500

    asd
    sdfsd

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 7777,
-    DB_CONNECT: 'mongodb+srv://bruce:HX!HetN67jV@cluster0.zcm24.mongodb.net/test_work',
+    DB_CONNECT: 'mongodb+srv://abdulhay7772019:aNXyH78WqBK6zQ4J@taskpal.tjiimop.mongodb.net/test-app',
     JWT: 'dev',
     JWT_time: (60 * 60) * 24,
     corsOptions: {
